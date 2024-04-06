2e2cc0bbbd68c96344f777d1e23043e4db268cbb
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2024-04-06 07:53:48 +0100

    Debut du versionning: V1.0.0

--- src/main/java/com/psi/project_psi/configurations/WebConfig.java
+++ src/main/java/com/psi/project_psi/configurations/WebConfig.java
@@ -2,6 +2,8 @@ package com.psi.project_psi.configurations;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:./uploads/", "file:./well-know/");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("com.psi.project_psi.controller"));
     }
 
 }
